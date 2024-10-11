c582fcdded1438f5b639362b93b9e113a5551c5b
aungthuhein2005 <aungthuhein.ath05@gmail.com>
2024-10-11 14:22:19 +0630

    image upload

--- banana-writer/src/TextEditor.jsx
+++ banana-writer/src/TextEditor.jsx
@@ -13,14 +13,103 @@ const TextEditor = ({ noteToOpen, setNoteToDisplay }) => {
   });
 
   const [isDropdownVisible, setDropdownVisible] = useState(false);
-  const execCommand = (command, value = null) => {
-    
-    if (document.queryCommandSupported(command)) {
-      document.execCommand(command, false, value);
-    } else {
-      console.error(`${command} is not supported`);
+  
+  const execCommand = (command,file) => {
+    if (command === "code") {
+      const selection = window.getSelection();
+      if (selection.rangeCount > 0) {
+        const range = selection.getRangeAt(0);
+        const selectedText = range.toString();
+  
+        if (selectedText) {
+          const selectedNode = range.commonAncestorContainer.nodeType === 3
+            ? range.commonAncestorContainer.parentNode
+            : range.commonAncestorContainer;
+  
+          // Toggle <code> tag
+          if (selectedNode.nodeName === "CODE") {
+            const parent = selectedNode.parentNode;
+            while (selectedNode.firstChild) {
+              parent.insertBefore(selectedNode.firstChild, selectedNode);
+            }
+            parent.removeChild(selectedNode);
+          } else {
+            const codeElement = document.createElement("code");
+            codeElement.textContent = selectedText;
+            range.deleteContents();
+            range.insertNode(codeElement); // Insert <code> tag
+          }
+        }
+      }
+    }else if (command === "quote") {
+      const selection = window.getSelection();
+      if (selection.rangeCount > 0) {
+        const range = selection.getRangeAt(0);
+        const selectedText = range.toString();
+  
+        if (selectedText) {
+          const selectedNode = range.commonAncestorContainer.nodeType === 3
+            ? range.commonAncestorContainer.parentNode
+            : range.commonAncestorContainer;
+  
+          // Toggle <blockquote> tag
+          if (selectedNode.nodeName === "BLOCKQUOTE") {
+            const parent = selectedNode.parentNode;
+            while (selectedNode.firstChild) {
+              parent.insertBefore(selectedNode.firstChild, selectedNode);
+            }
+            parent.removeChild(selectedNode); // Remove <blockquote> tag
+          } else {
+            const blockquoteElement = document.createElement("blockquote");
+            blockquoteElement.textContent = selectedText;
+            range.deleteContents();
+            range.insertNode(blockquoteElement); // Insert <blockquote> tag
+          }
+        }
+      }
+    }else  if (command === "separator") {
+    const selection = window.getSelection();
+    if (selection.rangeCount > 0) {
+      const range = selection.getRangeAt(0);
+
+      // Create a horizontal rule
+      const hrElement = document.createElement("hr");
+      
+      // Move the range to the end of the selection
+      range.collapse(false); // Collapse the range to the end (false means the end)
+      range.insertNode(hrElement); // Insert <hr> element at the end of the selection
+
+      // Optionally, move the cursor after the inserted <hr> element
+      range.collapse(false);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+    }else if (command === "image" && file) {
+      const selection = window.getSelection();
+      if (selection.rangeCount > 0) {
+        const range = selection.getRangeAt(0);
+        if (file) {
+          const reader = new FileReader();
+          // console.log(reader.result);
+
+          reader.onloadend = () => {
+            const imgElement = document.createElement("img");
+            imgElement.src = reader.result; // Set the src to the result of FileReader
+            imgElement.style.maxWidth = "100%"; // Optional: limit the image width
+
+            range.collapse(false); // Collapse the range to the end (false means the end)
+            range.insertNode(imgElement); // Insert the image element at the end of the selection
+          };
+          reader.readAsDataURL(file); 
+      };
+    }
+    }  else {
+      document.execCommand(command, false, null); // For other commands
     }
   };
+  
+
+ 
   
   const handleSelectionChange = () => {
     const selection = document.getSelection();
--- banana-writer/src/Toolbar.jsx
+++ banana-writer/src/Toolbar.jsx
@@ -1,5 +1,5 @@
 import "./TextEditor.css";
-import { FaBold } from "react-icons/fa6";
+import { FaBold, FaVideo } from "react-icons/fa6";
 import { FaItalic } from "react-icons/fa";
 import { FaUnderline } from "react-icons/fa";
 import { FaPalette } from "react-icons/fa";
@@ -11,10 +11,23 @@ import { FaAlignRight } from "react-icons/fa";
 import { FaAlignCenter } from "react-icons/fa6";
 import { FaAlignJustify } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
-import { useState } from "react";
+import { MdFormatColorText } from "react-icons/md";
+import { FaCode } from "react-icons/fa6";
+import { BsBlockquoteLeft } from "react-icons/bs";
+import { RiSeparator } from "react-icons/ri";
+import { useEffect, useRef, useState } from "react";
+import { IoIosMore } from "react-icons/io";
+import { FaImage } from "react-icons/fa";
+import { IoMdMusicalNotes } from "react-icons/io";
+import { FaFile } from "react-icons/fa";
+import { FaFileVideo } from "react-icons/fa";
 
-function Toolbar({ position, execCommand,setDropdownVisible }) {
+function Toolbar({ position, execCommand, setDropdownVisible }) {
   const [isFontSizeToolVisible, setFontSizeToolVisible] = useState(false);
+  const [isOtherToolVisible, setOtherToolVisible] = useState(false);
+  const colorPickerRef = useRef();
+  const textBgPicker = useRef();
+  const imgInputRef = useRef();
 
   const toolbarStyle = {
     position: "absolute",
@@ -25,40 +38,102 @@ function Toolbar({ position, execCommand,setDropdownVisible }) {
     borderRadius: 4,
   };
 
-  
-  const toggleFontSizeTool = (e) => {
+  const toggleColorPicker = () => {
+    colorPickerRef.current.click();
+    const color = colorPickerRef.current.value;
+    execCommand("foreColor", color);
+  }
+
+  const toggleTextBgPicker = () => {
+    colorPickerRef.current.click();
+    const color = colorPickerRef.current.value;
+    execCommand("hiliteColor", color);
+  }
+
+  const toggleOtherTool = () => {
+    setOtherToolVisible(!isOtherToolVisible);
+    setDropdownVisible(!isOtherToolVisible);
+  }
+
+  const toggleFontSizeTool = () => {
     setFontSizeToolVisible(!isFontSizeToolVisible);
-    setDropdownVisible(!isFontSizeToolVisible); // Ensure this keeps the toolbar open
+    setDropdownVisible(!isFontSizeToolVisible);
   };
-  
+
+  const toggleImgInput = () => {
+    imgInputRef.current.click();
+    
+  }
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    execCommand("image",file)
+  };
 
   return (
     <div style={toolbarStyle} className="toolbar">
       <ul className="toolbar-list">
-        <button className="tool" onClick={() =>execCommand("bold")}><FaBold size={12}  /></button>
-        <button className="tool" onClick={() => execCommand("italic")}><FaItalic size={12}  /></button>
-        <button className="tool" onClick={() => execCommand("underline")}><FaUnderline size={12}  /></button>
-        <button className="tool"><FaFillDrip size={12} /></button>
-        <button className="tool"><FaPalette size={12} /></button>
+        <button className="tool" onClick={() => execCommand("bold")}><FaBold size={12} /></button>
+        <button className="tool" onClick={() => execCommand("italic")}><FaItalic size={12} /></button>
+        <button className="tool" onClick={() => execCommand("underline")}><FaUnderline size={12} /></button>
+        <button className="tool" onClick={toggleTextBgPicker}><FaFillDrip size={12} /></button>
+        <input type="color" style={{ display: 'none' }} ref={textBgPicker} />
+        <button className="tool" onClick={toggleColorPicker}><MdFormatColorText size={12} /></button>
+        <input type="color" style={{ display: 'none' }} ref={colorPickerRef} />
         <button className="tool"><FaLink size={12} /></button>
-        <button className="tool"><FaAlignLeft size={12} /></button>
-        <button className="tool"><FaAlignCenter size={12} /></button>
-        <button className="tool"><FaAlignJustify size={12} /></button>
-        <button className="tool"><FaAlignRight size={12} /></button>
+        <button className="tool" onClick={() => execCommand("justifyLeft")}><FaAlignLeft size={12} /></button>
+        <button className="tool" onClick={() => execCommand("justifyCenter")}><FaAlignCenter size={12} /></button>
+        <button className="tool" onClick={() => execCommand("justifyFull")}><FaAlignJustify size={12} /></button>
+        <button className="tool" onClick={() => execCommand("justifyRight")}><FaAlignRight size={12} /></button>
         <button className="tool" onClick={toggleFontSizeTool} style={{ position: 'relative' }}>
           <span style={{ marginRight: '5px' }}>Text</span>
-          <IoIosArrowDown size={12}  style={{ cursor: "pointer" }} />
+          <IoIosArrowDown size={12} style={{ cursor: "pointer" }} />
           {isFontSizeToolVisible && (
             <ul className="font-size-tool">
-              <li onClick={() => execCommand("formatBlock", "<h3>")}><button><h3>Heading 1</h3></button></li>
-              <li onClick={() => execCommand("formatBlock", "<h4>")}><button><h4>Heading 2</h4></button></li>
-              <li onClick={() => execCommand("formatBlock", "<h5>")}><button><h5>Heading 3</h5></button></li>
-              <li onClick={() => execCommand("formatBlock", "<p>")}><button><p>Paragraph</p></button></li>
+              <li onClick={() => execCommand("formatBlock", "<h3>")}>
+                <div><h3>Heading 1</h3></div>
+              </li>
+              <li onClick={() => execCommand("formatBlock", "<h4>")}>
+                <div><h4>Heading 2</h4></div>
+              </li>
+              <li onClick={() => execCommand("formatBlock", "<h5>")}>
+                <div><h5>Heading 3</h5></div>
+              </li>
+              <li onClick={() => execCommand("formatBlock", "<p>")}>
+                <div><p>Paragraph</p></div>
+              </li>
             </ul>
           )}
         </button>
 
-        <button className="tool" onClick={()=>execCommand("removeFormat")}><ImClearFormatting size={12} onClick={() => execCommand("removeFormat")} /></button>
+        <button className="tool" onClick={() => execCommand("quote")}><BsBlockquoteLeft size={14} /></button>
+        <button className="tool" onClick={() => execCommand("code")}><FaCode size={14} /></button>
+        <button className="tool" onClick={() => execCommand("separator")}><RiSeparator size={14} /></button>
+        <input type="file" accept="image/*" onChange={handleImageChange} style={{ display: 'none' }} ref={imgInputRef} />
+              <button className="tool" onClick={toggleImgInput}>
+                <div><i><FaImage /></i></div>
+              </button>
+        {/* <button className="tool" onClick={toggleOtherTool} style={{ position: 'relative' }}>
+          <IoIosMore size={12} style={{ cursor: "pointer" }} />
+          {isOtherToolVisible && (
+            <ul className="file-tool">
+                <input type="file" accept="image/*" onChange={handleImageChange} style={{ display: 'none' }} ref={imgInputRef} />
+              <li onClick={toggleImgInput}>
+                <div><i><FaImage /></i> <span>Image</span></div>
+              </li>
+              <li onClick={() => execCommand("song")}>
+                <div><i><IoMdMusicalNotes /></i> <span>Song</span></div>
+              </li>
+              <li onClick={() => execCommand("video")}>
+                <div><i><FaVideo /></i> <span>Video</span></div>
+              </li>
+              <li onClick={() => execCommand("file")}>
+                <div><i><FaFile /></i> <span>File</span></div>
+              </li>
+            </ul>
+          )}
+        </button> */}
+        <button className="tool" onClick={() => execCommand("removeFormat")}><ImClearFormatting size={12} /></button>
       </ul>
     </div>
   );
